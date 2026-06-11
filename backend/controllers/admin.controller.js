01ede45036ba66c58aae6a5525061856c1b46e66
princeraj18 <princekr497@gmail.com>
2026-06-11 15:19:27 +0530

    deploy backend

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -1,4 +1,4 @@
-import Admin from "../models/admin.model.js";
+import Admin from "../models/Admin.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
